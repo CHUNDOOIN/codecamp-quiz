0ff5fea5a68b4b231f662e0a6cd864e56fa09413
CHUNDOOIN <chun.doo.in@gmail.com>
2022-03-22 19:02:47 +0900

    22.03.22 퀴즈

---
+++ pages/quiz-22.03.22/01-filter-map/index.js
@@ -0,0 +1,17 @@
+const classmates = [
+  { name: "철수", age: 10, school: "토끼초등학교" },
+  { name: "영희", age: 13, school: "다람쥐초등학교" },
+  { name: "훈이", age: 11, school: "토끼초등학교" },
+];
+
+// 보너스1
+
+// (1)
+classmates
+  .filter((el) => el.school === "토끼초등학교")
+  .map((el) => ({ name: el.name, age: el.age, candy: 10 }));
+
+// (2)
+classmates
+  .filter((el) => el.school === "다람쥐초등학교")
+  .map((el) => ({ name: el.name + "어린이", age: el.age }));
